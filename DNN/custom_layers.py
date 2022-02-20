aed203458b27f48a4b0d173555c4451e2b903b67
Stefano <stefanodonne@gmail.com>
2022-02-20 19:25:02 +0100

    Fix augmentation layer bug

--- DNN/custom_layers.py
+++ DNN/custom_layers.py
@@ -3,12 +3,11 @@ import numpy as np
 
 
 class Random90Rotation(tf.keras.layers.Layer):
-    def __init__(self, movements=[0, 1, 2, 3], **kwargs):
+    def __init__(self, **kwargs):
         super(Random90Rotation, self).__init__(**kwargs)
-        self.movements = movements
 
     def call(self, images, training=None):
         if not training:
             return images
-        images = tf.image.rot90(images, k=np.random.choice(self.movements))
+        images = tf.image.rot90(images, k=np.random.choice([0, 1, 2, 3]))
         return images
