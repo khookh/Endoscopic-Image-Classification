f46bb8b41ce0c948dc5330a2f2589e586438f56f
Stefano <stefanodonne@gmail.com>
2022-02-21 23:46:15 +0100

    New tests

--- DNN/custom_layers.py
+++ DNN/custom_layers.py
@@ -11,3 +11,31 @@ class Random90Rotation(tf.keras.layers.Layer):
             return images
         images = tf.image.rot90(images, k=np.random.choice([0, 1, 2, 3]))
         return images
+
+
+class RandomHue(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super(RandomHue, self).__init__(**kwargs)
+
+    def call(self, images, training=None):
+        if not training:
+            return images
+        a = -1.5/180 # hue levels are between 0 and 180, standard deviation observed is about 3
+        b = -a
+        images = tf.image.adjust_hue(images, ((b - a) * np.random.random_sample() + a))
+        return images
+
+
+class RandomGaussian(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super(RandomGaussian, self).__init__(**kwargs)
+
+    def call(self, images, training=None):
+        if not training:
+            return images
+        prob = abs(np.random.normal(0,1))
+        if 0.4 < prob < 0.8:
+            images = tf.gaussian.GaussianBlur(size=3)
+        elif prob >= 0.8:
+            images = tf.gaussian.GaussianBlur(size=5)
+        return images
