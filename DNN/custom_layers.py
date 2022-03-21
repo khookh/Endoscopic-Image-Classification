86179841e55dea8cdddee01a6a41103cbd86a194
sdonne <stefanodonne@gmail.com>
2022-03-21 11:42:35 +0100

    Adds gaussian smoothing

--- DNN/custom_layers.py
+++ DNN/custom_layers.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow_addons as tfa
 import numpy as np
 
 
@@ -20,7 +21,7 @@ class RandomHue(tf.keras.layers.Layer):
     def call(self, images, training=None):
         if not training:
             return images
-        a = -1.5/180 # hue levels are between 0 and 180, standard deviation observed is about 3
+        a = -1.5 / 180  # hue levels are between 0 and 180, standard deviation observed is about 3
         b = -a
         images = tf.image.adjust_hue(images, ((b - a) * np.random.random_sample() + a))
         return images
@@ -30,12 +31,16 @@ class RandomGaussian(tf.keras.layers.Layer):
     def __init__(self, **kwargs):
         super(RandomGaussian, self).__init__(**kwargs)
 
+    # def call(self, images, training=None):
+    #    if not training:
+    #        return images
+    #    prob = abs(np.random.normal(0,1))
+    #    if 0.4 < prob < 0.8:
+    #        images = tf.gaussian.GaussianBlur(size=3)
+    #    elif prob >= 0.8:
+    #        images = tf.gaussian.GaussianBlur(size=5)
+    #    return images
     def call(self, images, training=None):
         if not training:
             return images
-        prob = abs(np.random.normal(0,1))
-        if 0.4 < prob < 0.8:
-            images = tf.gaussian.GaussianBlur(size=3)
-        elif prob >= 0.8:
-            images = tf.gaussian.GaussianBlur(size=5)
-        return images
+        return tfa.image.gaussian_filter2d(image = images, filter_shape=(3,3))
