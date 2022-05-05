337cf5ac6d15d56a678f0c721d5db3c755441f76
Stefano <stefanodonne@gmail.com>
2022-05-05 16:23:50 +0200

    Update heatmap script to work with InceptionV3

--- DNN/heatmap_script.py
+++ DNN/heatmap_script.py
@@ -13,15 +13,16 @@ batch_size = 32
 img_height = 500
 img_width = 500
 
-INPUT_PATH = 'D:\\Thesis\\DataSet_cropped'
+#INPUT_PATH = 'D:\\Thesis\\DataSet_cropped'
+INPUT_PATH = 'D:\\Thesis\\kvasir-dataset-v2\\train'
 OUTPUT_PATH = 'D:\\Thesis\\Heatmaps'
 
 tf.config.list_physical_devices('GPU')
-MODEL_PATH = 'MODEL_DENSE121'
+MODEL_PATH = 'MODEL_INCEPTIONV3'
 labels = ['angle', 'greater_curvature', 'junction', 'oesophagus', 'pylore_antre', 'retro_vision', 'unqualified']
 
 model = tf.keras.models.load_model(MODEL_PATH)
-densenet121 = model.layers[3]
+inceptionv3 = model.layers[3]
 
 def text_on_frame(dispframe_, site):
     """
@@ -37,8 +38,8 @@ def text_on_frame(dispframe_, site):
 
 def heatmap_from_pred(x):
     with tf.GradientTape() as tape:
-        last_conv_layer = densenet121.get_layer('relu')#('conv5_block16_concat')#('conv5_block16_2_conv')
-        iterate = tf.keras.models.Model([densenet121.inputs], [densenet121.output, last_conv_layer.output])
+        last_conv_layer = inceptionv3.get_layer('conv2d_93')#('conv5_block16_concat')#('conv5_block16_2_conv')
+        iterate = tf.keras.models.Model([inceptionv3.inputs], [inceptionv3.output, last_conv_layer.output])
         model_out, last_conv_layer = iterate(x)
         #print(model_out.shape,model_out)
         class_out = model_out[:,:,: np.argmax(model_out[0])]
@@ -51,7 +52,7 @@ def heatmap_img(img, sauce):
     heatmap = heatmap_from_pred(img[None])
     heatmap = np.maximum(heatmap, 0)
     heatmap /= np.max(heatmap)
-    heatmap = heatmap.reshape((15, 15))
+    heatmap = heatmap.reshape((14, 14))
     heatmap = cv.resize(heatmap, (sauce.shape[1], sauce.shape[0]))
     heatmap = cv.applyColorMap(np.uint8(255 * heatmap), cv.COLORMAP_JET)
     heatmap = heatmap * 0.5 + sauce
@@ -82,9 +83,15 @@ if __name__ == '__main__':
                 quit = True
                 break
             if k == ord('s'):
-                p = OUTPUT_PATH+"\\"+picked_site+"_"+site+".png"
-                cv.imwrite(p,hm)
+                frame_count = 0
+                p = OUTPUT_PATH+"\\KVASIR_"+picked_site+"_"+site
+                while os.path.exists(f'{p}_{frame_count}.png'):
+                    frame_count += 1
+                cv.imwrite(f'{p}_{frame_count}.png',hm*255)
+                cv.imwrite(f'{p}_{frame_count}_blk.png', img)
                 print(f'image saved as {p}')
                 break
+
+        cv.destroyWindow('classification')
 
 
